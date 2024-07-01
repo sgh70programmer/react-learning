405147dea8397923dbdc30140e15ab7be9f0134d
sadegh <sadegh_ghorbani@yahoo.com>
2024-07-01 18:10:32 +0330

    class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>it is {new Date().toLocaleTimeString()} </h2>
+    )
+  }
+}
 
+class Hello extends React.Component{
+  render(){
+    return(
+      <h1>سلام دوستان من </h1>
+    )
+  }
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+class App extends React.Component{
+  render(){
+    return(
+      <>
+        <Hello/>
+        <Timer/>
+      </>
+    )
+  }
+}
+
+const tick = () =>{
+  ReactDOM.render(<App />, document.getElementById("root"))
+}
+
+setInterval(tick, 1000)
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
   
-    <App />
+//     <App />
  
   
-);
+// );
 
 
