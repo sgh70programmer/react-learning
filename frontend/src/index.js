5360bdfde1cc076af8074c4046e0059763f9a7f0
sadegh <sadegh_ghorbani@yahoo.com>
2024-07-01 19:06:24 +0330

    component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-class Timer extends React.Component{
-  render(){
-    return(
-      <h2>it is {new Date().toLocaleTimeString()} </h2>
-    )
-  }
-}
 
-class Hello extends React.Component{
-  render(){
-    return(
-      <h1>سلام دوستان من </h1>
-    )
-  }
-}
 
-class App extends React.Component{
-  render(){
-    return(
-      <>
-        <Hello/>
-        <Timer/>
-      </>
-    )
-  }
-}
 
-const tick = () =>{
-  ReactDOM.render(<App />, document.getElementById("root"))
-}
 
-setInterval(tick, 1000)
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-  
-//     <App />
- 
-  
-// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+  <App />
+
+
+);
 
 
